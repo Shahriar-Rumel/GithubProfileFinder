6dc4ede2005513e99d7b1b1f6deff9cd371a0d9c
Shahriar-Rumel <kenkanekikunsr@gmail.com>
2021-03-29 21:33:52 +0600

    On branch master

--- github.js
+++ github.js
@@ -1,21 +1,21 @@
 class GitHub{
     constructor(){
-        // this.client_id ='00ade7b7bc5ddafcc555';
-        // this.client_secret ='3e037eeaa35d6ed413dbd00ff5716ce550b4abfa';
+        this.client_id ='45ac13038e83afe75a29';
+        this.client_secret ='04c586a3baa204a49abdf6ba83c6fc093660243b';
         this.repos_count =5;
         this.repos_sort='created: asc';
     }
     async getUser(user){
         
         const headers ={
-            "Authorization" : `Token 15d8b875a1969d8c4d30ddbc14fd73b8226763ba`
+            "Authorization" : `Token b79dee938abe6df0ae9802aaf93c48e9b18c0e29`
         }
-        const url = `https://api.github.com/users/${user}`;
+        const url = `https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`;
         const profileResponse = await fetch(url,{
            "method" :"GET",
             "headers" :headers
         });
-        const urlr = `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}`;
+        const urlr = `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`;
         const repoResponse = await fetch(urlr,
             {
                 "method" :"GET",
