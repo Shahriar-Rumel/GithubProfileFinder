1eb5de40443c6c24c1d3b4b4993899c7c10d29d9
Rumel <71602274+Shahriar-Rumel@users.noreply.github.com>
2021-03-29 20:29:44 +0600

    Update github.js

--- github.js
+++ github.js
@@ -1,7 +1,7 @@
 class GitHub{
     constructor(){
-        this.client_id ='00ade7b7bc5ddafcc555';
-        this.client_secret ='3e037eeaa35d6ed413dbd00ff5716ce550b4abfa';
+//         this.client_id ='00ade7b7bc5ddafcc555';
+//         this.client_secret ='3e037eeaa35d6ed413dbd00ff5716ce550b4abfa';
         this.repos_count =5;
         this.repos_sort='created: asc';
     }
@@ -9,7 +9,7 @@ class GitHub{
     async getUser(user){
         
         const headers ={
-            "Authorization" : `Token 8475738243758803a2f53e286681a2af094bca03`
+            "Authorization" : `Token 5eda0133e65a8d1d0c7525699c6b6bbb5731a757`
         }
         const url = `https://api.github.com/users/${user}`;
         const profileResponse = await fetch(url,{
